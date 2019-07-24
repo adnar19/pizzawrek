0a8673052385fd27457c27682f31bc69777d7148
randa <renda.kouachi@gmail.com>
2019-07-25 00:18:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+include 'nav.php';
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mdl/material.min.css">
+    <script src="mdl/material.min.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<style >
+
+body {
+background: #ffecb3;
+position: relative;
+}
+</style>
+    <title></title>
+  </head>
+  <body>
+
+<div class="">
+
+
+
+    <div class="demo-card-square mdl-card mdl-shadow--2dp" style="margin-left: 20px;margin-top: 20px; width: 150px; height: 150px;">
+      <div class="mdl-card__title mdl-card--expand" style="color:white;background:url('images/pizza4.jpg') bottom right 40% no-repeat;">
+        <h2 class="mdl-card__title-text" >Pizzen</h2>
+      </div>
+      <div class="mdl-card__supporting-text">
+      Alle Pizzen sind mit Tomatensauce, Goudakase und Oregano
+      </div>
+      <div class="mdl-card__actions mdl-card--border" style="background:#ef5350;">
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+     voir options
+        </a>
+      </div>
+    </div>
+
+
+        <div class="demo-card-square mdl-card mdl-shadow--2dp" style="margin-left:200px;margin-top: 20px; width: 150px; height: 150px;">
+          <div class="mdl-card__title mdl-card--expand" style="color:white;background:url('images/pasta.jpg') bottom right 40% no-repeat;">
+            <h2 class="mdl-card__title-text" >Nudeln</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+          Alle Pizzen sind mit Tomatensauce, Goudakase und Oregano
+          </div>
+          <div class="mdl-card__actions mdl-card--border" style="background:#ef5350;">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+         voir options
+            </a>
+          </div>
+        </div>
+
+
+
+  </div>
+
+
+
+
+  </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+
+<div style="width:100%;height:60px;background:#ffeb3b;">
+<button id=""
+        class="mdl-button mdl-js-button mdl-button--icon" style="margin-right:230px;margin-top:10px;">
+  <i class="material-icons">navigate_before</i>
+</button>
+
+  <button id=""
+          class="mdl-button mdl-js-button mdl-button--icon" style="margin-top:10px;">
+    <i class="material-icons">add_shopping_cart</i>
+  </button>
+  <button id="demo-menu-lower-right"
+          class="mdl-button mdl-js-button mdl-button--icon bp"style="margin-left:10px;margin-top:10px;">
+    <i class="material-icons">more_vert</i>
+  </button>
+
+  <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+      for="demo-menu-lower-right" >
+    <li class="mdl-menu__item">Some Action</li>
+    <li class="mdl-menu__item">Another Action</li>
+  </ul>
+</div>
